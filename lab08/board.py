6a445ba7963856cda0346b004d768666d02e14c5
Freddieboyy <ksfredrikolofsson@gmail.com>
2023-03-26 15:50:09 +0200

    upd

--- lab08/board.py
+++ lab08/board.py
@@ -21,20 +21,54 @@ class TicTacToeBoard:
         else: return False
 
     def place(self,marker, row, col):
-        pass
+        if self.is_empty([row][col]):
+            self.board[row][col] = marker
+            return True
+        else:
+            return False
 
     def is_full(self):
-        pass
+        counter = 0
+        for n,lista in enumerate(self.board):
+            for k,val in enumerate(lista):
+                if not self.is_empty[n][k]:
+                    counter += 1
+        
+        if counter == 9:
+            return True
+        else: 
+            return False
+                
 
 
     def is_winner(self,marker):
-        pass
+   
+        #Rows
+        for row in self.board:
+            if len(set(row)) == 1:
+                return True
+        #Columns
+        
+        for row in self.board:
+            if self.board[row][0] != marker:
+                return False
+            elif self.board[row][1] != marker:
+                return False
+            elif self.board[row][2] != marker:
+                return False
+        return True
+                
+            
+            
+            
 
     def restart(self):
-        pass
+        self.board = [['-','-','-'],
+                      ['-','-','-'],
+                      ['-','-','-']]
 
     def print_board(self):
-        pass
+        print(f"board: {self.board}")
 
 board = TicTacToeBoard()
 print(board.is_empty(1,1))

37c8e24f00da2903b1af7f9ff588ce85e6d2dfed
Freddieboyy <ksfredrikolofsson@gmail.com>
2023-03-26 11:50:33 +0200

    Update obj_uppg5.py

--- lab03/obj_uppg5.py
+++ lab03/obj_uppg5.py
@@ -10,6 +10,16 @@ class Padda:
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
+        self.padda1.shape('turtle')
+        self.padda2.shape('turtle')
+        self.padda1.pendown()
+        self.padda1.goto(self.x1, self.y1)
+        self.padda1.pencolor('red')
+        self.padda2.pendown()
+        self.padda2.goto(self.x2, self.y2)
+        self.padda2.pencolor('blue')
+        turtle.Screen().bgcolor('green')
+
         
     def distance(self, x1,x2,y1,y2):
         dist = math.sqrt((x2-x1)**2+(y2-y1)**2)
@@ -20,15 +30,7 @@ class Padda:
         return step
     
     def random_walk(self):
-        self.padda1.pendown()
-        self.padda1.goto(self.x1, self.y1)
-        
-        self.padda2.pendown()
-        self.padda2.goto(self.x2, self.y2)
-        
         while self.distance(self.x1, self.x2,self.y1, self.y2) != 0:
-           
-           
             degree = random.randint(-90,90)
             left_right_padda1 = random.randint(0,1)
             left_right_padda2 = random.randint(0,1)

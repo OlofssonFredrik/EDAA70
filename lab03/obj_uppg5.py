3f690a9d5cd97eb6bf83a17c129ccf859da05da0
Freddieboyy <ksfredrikolofsson@gmail.com>
2023-03-26 11:59:44 +0200

    Update obj_uppg5.py

--- lab03/obj_uppg5.py
+++ lab03/obj_uppg5.py
@@ -18,6 +18,7 @@ class Padda:
         self.padda2.pendown()
         self.padda2.goto(self.x2, self.y2)
         self.padda2.pencolor('blue')
+        turtle.Screen().delay(0.1)
         turtle.Screen().bgcolor('green')
 
         
@@ -31,28 +32,30 @@ class Padda:
     
     def random_walk(self):
         while self.distance(self.x1, self.x2,self.y1, self.y2) != 0:
-            degree = random.randint(-90,90)
-            left_right_padda1 = random.randint(0,1)
-            left_right_padda2 = random.randint(0,1)
-            
-            if left_right_padda1 > 0.5:
-                self.padda1.forward(self.random_step(1,10))
-                self.padda1.right(degree)
-            else:
-                self.padda1.forward(self.random_step(1,10))
-                self.padda1.left(degree)
+            if self.distance(self.x1, self.x2,self.y1, self.y2) < 300:
+                degree = random.randint(-90,90)
+                left_right_padda1 = random.randint(0,1)
+                left_right_padda2 = random.randint(0,1)
                 
-            if left_right_padda2 > 0.5:
-                self.padda2.forward(self.random_step(1,10))
-                self.padda2.right(degree)
-            else:
-                self.padda2.forward(self.random_step(1,10))
-                self.padda2.left(degree)
-            
+                if left_right_padda1 > 0.5:
+                    self.padda1.forward(self.random_step(1,10))
+                    self.padda1.right(degree)
+                else:
+                    self.padda1.forward(self.random_step(1,10))
+                    self.padda1.left(degree)
+                    
+                if left_right_padda2 > 0.5:
+                    self.padda2.forward(self.random_step(1,10))
+                    self.padda2.right(degree)
+                else:
+                    self.padda2.forward(self.random_step(1,10))
+                    self.padda2.left(degree)
+                
             self.x1 = self.padda1.xcor()
             self.x2 = self.padda1.ycor()
             self.y1 = self.padda2.xcor()
             self.y2 = self.padda2.ycor()
+ 
             
         turtle.mainloop()
 

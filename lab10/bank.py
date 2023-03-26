112825ebd859831f0008b98076b49b5474b7b7f4
Freddieboyy <ksfredrikolofsson@gmail.com>
2023-03-26 17:54:49 +0200

    upd

--- lab10/bank.py
+++ lab10/bank.py
@@ -1,35 +1,15 @@
-class Account:
-    def __init__(self, customer_id, account_nbr):
-        self._customer_id = customer_id
-        self._account_nbr = account_nbr
-        self._balance = 0
+from account import Account
 
-    def get_customer_id(self):
-        return self._customer_id
-
-    def get_account_nbr(self):
-        return self._account_nbr
-
-    def get_balance(self):
-        return self._balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self._balance += amount
-    
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self._balance:
-            self._balance -= amount
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return (
-            f'Account(owner: {self._customer_id}, '
-            f'nbr: {self._account_nbr}, '
-            f'balance: {self._balance})'
-        )
-
-    def __repr__(self):
-        return str(self)
+class Bank:
+    def __init__(self) -> None:
+        pass
+        
+        self.customers = []
+    def add_customer(self,name):
+        for k,customer in enumerate(self.customers):
+            lista = []
+        
+        print(k)
+        
+            
+Bank().add_customer()

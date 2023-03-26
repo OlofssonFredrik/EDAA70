f91f36b71ffeccc31d90d7a7096b281953886bb4
Freddieboyy <ksfredrikolofsson@gmail.com>
2023-03-26 16:34:37 +0200

    upd

--- lab08/board.py
+++ lab08/board.py
@@ -18,12 +18,14 @@ class TicTacToeBoard:
     def is_empty(self,row, col):
         if self.board[row][col] == '-':
             return True
-        else: return False
+        else: 
+            return False
+        
 
     def place(self,marker, row, col):
-        if self.is_empty([row][col]):
+        if self.is_empty(row,col):
             self.board[row][col] = marker
-            return True
+            return self.board
         else:
             return False
 
@@ -31,7 +33,7 @@ class TicTacToeBoard:
         counter = 0
         for n,lista in enumerate(self.board):
             for k,val in enumerate(lista):
-                if not self.is_empty[n][k]:
+                if not self.is_empty(n,k):
                     counter += 1
         
         if counter == 9:
@@ -45,22 +47,29 @@ class TicTacToeBoard:
    
         #Rows
         for row in self.board:
-            if len(set(row)) == 1:
+     
+            if marker in set(row) and len(set(row)) == 1:
                 return True
         #Columns
+        for col in range(3):
+            checker = []
+            for row in range(3):
+                checker.append(self.board[row][col])
+            if marker in set(checker) and len(set(checker)) == 1:
+                return True
+    
+    
+        #Diagonaler
+        diagonal1 = [self.board[i][i] for i in range(len(self.board))]
+        if marker in set(diagonal1) and len(set(diagonal1)) == 1:
+            return True
+            
+        diagonal2 = [self.board[i][len(self.board)-1-i] for i in range(len(self.board))]
+        if marker in set(diagonal2) and len(set(diagonal2)) == 1:
+            return True
         
-        for row in self.board:
-            if self.board[row][0] != marker:
-                return False
-            elif self.board[row][1] != marker:
-                return False
-            elif self.board[row][2] != marker:
-                return False
-        return True
+        return False
                 
-            
-            
-            
 
     def restart(self):
         self.board = [['-','-','-'],
@@ -70,5 +79,16 @@ class TicTacToeBoard:
     def print_board(self):
         print(f"board: {self.board}")
 
-board = TicTacToeBoard()
-print(board.is_empty(1,1))
+
+
+
+if __name__ == '__main__':
+
+    b = TicTacToeBoard()
+    b.place('O', 0, 0)
+    b.place('O', 0, 1)
+    b.place('O', 0, 2)
+    print(b.board)
+    print(b.is_winner('O'))
+    print(b.restart())
+    print(b.board)
